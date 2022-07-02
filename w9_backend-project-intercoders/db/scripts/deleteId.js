a93cb9f8c92283c1f07ce29123ae7cbdd0547957
ghavasi <g.havasi@outlook.com>
2022-07-02 15:20:50 +0100

    Documentation was added

--- w9_backend-project-intercoders/db/scripts/deleteId.js
+++ w9_backend-project-intercoders/db/scripts/deleteId.js
@@ -1,12 +1,12 @@
 import { query } from "../index.js";
 
 //delete the student from the database
-const sqlQuery = `DELETE FROM waitinglist WHERE name is null`;
+const sqlQuery = `DELETE FROM announcement WHERE id = '9' `;
 
 async function waitingList() {
   console.log("Hello");
   const res = await query(sqlQuery);
-  console.log("Table was created");
+  console.log("it was deleted");
 }
 
 waitingList();
